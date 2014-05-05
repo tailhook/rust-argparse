9e9773e3adc7690b4ab1a8c9876a6f94a121a60f
Paul Colomiets <paul@colomiets.name>
2014-05-05 22:40:09 +0300

    Add example file

---
+++ examples/greeting.rs
@@ -0,0 +1,38 @@
+/* The extern crate crashes current rust compiler (0.10) for some reason but
+ * directly including a module in program works */
+//extern crate argparse;
+extern crate collections;
+
+
+use std::os;
+
+use argparse::{ArgumentParser, StoreTrue, Store};
+
+#[path="../argparse/mod.rs"]
+mod argparse;
+
+fn main() {
+    let mut verbose = false;
+    let mut name = "World".to_owned();
+
+    let mut ap = ArgumentParser::new();
+    ap.set_description("Greet somebody.");
+    ap.refer(&mut verbose)
+        .add_option(["-v", "--verbose"], ~StoreTrue,
+        "Be verbose");
+    ap.refer(&mut name)
+        .add_option(["--name"], ~Store::<~str>,
+        "Name for the greeting");
+    match ap.parse_args() {
+        Ok(()) => {}
+        Err(x) => {
+            os::set_exit_status(x);
+            return;
+        }
+    }
+
+    if verbose {
+        println!("name is {}", name);
+    }
+    println!("Hello {}!", name);
+}
