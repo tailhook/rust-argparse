0f966268ab459e8451ec47ffd8e3027fdafccc5d
Paul Colomiets <paul@colomiets.name>
2014-07-06 19:57:26 +0000

    Add a test for StoreOption::<String>

--- argparse/test_optional.rs
+++ argparse/test_optional.rs
@@ -8,7 +8,7 @@ fn test_opt() {
     let mut ap = ArgumentParser::new();
     ap.refer(&mut val)
       .add_option(["-s", "--set"], box StoreOption::<int>,
-        "Set string value");
+        "Set int value");
     assert_eq!(val, None);
     check_ok(&ap, ["./argparse_test"]);
     assert_eq!(val, None);
@@ -16,5 +16,22 @@ fn test_opt() {
     assert_eq!(val, Some(10i));
     check_ok(&ap, ["./argparse_test", "--set", "11"]);
     assert_eq!(val, Some(11));
+    check_err(&ap, ["./argparse_test", "--set"]);
+}
+
+#[test]
+fn test_str() {
+    let mut val = None;
+    let mut ap = ArgumentParser::new();
+    ap.refer(&mut val)
+      .add_option(["-s", "--set"], box StoreOption::<String>,
+        "Set string value");
+    assert_eq!(val, None);
+    check_ok(&ap, ["./argparse_test"]);
+    assert_eq!(val, None);
+    check_ok(&ap, ["./argparse_test", "-s", "10"]);
+    assert_eq!(val, Some("10".to_string()));
+    check_ok(&ap, ["./argparse_test", "--set", "11"]);
+    assert_eq!(val, Some("11".to_string()));
     check_err(&ap, ["./argparse_test", "--set"]);
 }
