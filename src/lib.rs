bc9b73a9a9e952511de62c8e051438ed3ced555d
Paul Colomiets <paul@colomiets.name>
2015-01-15 21:35:29 +0200

    Fix floating point tests for rust alpha

--- src/lib.rs
+++ src/lib.rs
@@ -38,7 +38,7 @@ pub type StoreBool = Store<bool>;
 #[cfg(test)] mod test_parser;
 #[cfg(test)] mod test_bool;
 #[cfg(test)] mod test_int;
-//#[cfg(test)] mod test_float;
+#[cfg(test)] mod test_float;
 //#[cfg(test)] mod test_str;
 //#[cfg(test)] mod test_enum;
 //#[cfg(test)] mod test_pos;
--- src/test_float.rs
+++ src/test_float.rs
@@ -3,9 +3,8 @@ use super::{IncrBy,DecrBy};
 use super::Store;
 use test_parser::{check_ok,check_err};
 
-#[test]
-fn test_incr_decr() {
-    let mut val = 0.5;
+fn incr_decr(args: &[&str]) -> f32 {
+    let mut val = 0f32;
     {
         let mut ap = ArgumentParser::new();
         ap.refer(&mut val)
@@ -13,21 +12,39 @@ fn test_incr_decr() {
             "Decrement value")
           .add_option(&["-i", "--incr"], box IncrBy(0.5f32),
             "Increment value");
-        assert!(val == 0.5);
-        check_ok(&ap, &["./argparse_test",
-            "-iiddd", "--incr", "-iii"]);
+        check_ok(&ap, args);
     }
-    assert_eq!(val, 2.75);
+    return val;
+}
+
+#[test]
+fn test_incr_decr() {
+    assert_eq!(incr_decr(&["./argparse_test",
+        "--incr", "-iii"]), 2.0);
+    assert_eq!(incr_decr(&["./argparse_test",
+        "-iiddd", "--incr", "-iii"]), 2.25);
 }
 
 #[test]
 fn test_float() {
     let mut val = 0.1;
+    {
+        let mut ap = ArgumentParser::new();
+        ap.refer(&mut val)
+          .add_option(&["-s", "--set"], box Store::<f64>,
+            "Set float value");
+        check_ok(&ap, &["./argparse_test", "-s", "15.125"]);
+    }
+    assert_eq!(val, 15.125);
+}
+
+#[test]
+#[should_fail]
+fn test_fail() {
+    let mut val = 0.1;
     let mut ap = ArgumentParser::new();
     ap.refer(&mut val)
       .add_option(&["-s", "--set"], box Store::<f64>,
-        "Set integer value");
-    check_ok(&ap, &["./argparse_test", "-s", "15.125"]);
-    assert_eq!(val, 15.125);
-    check_err(&ap, &["./argparse_test", "-s", "test"]);
+        "Set float value");
+    check_ok(&ap, &["./argparse_test", "-s", "test"]);
 }
