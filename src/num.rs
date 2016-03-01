4c20997ee940d99ef1a5425e833e013787612065
Zachary Neely <zrneely@mit.edu>
2016-02-29 21:54:24 -0500

    Remove requirement for types in Actions to be Copy

--- src/num.rs
+++ src/num.rs
@@ -18,34 +18,40 @@ pub struct DecrByAction<'a, T: 'a> {
     cell: Rc<RefCell<&'a mut T>>,
 }
 
-impl<T: 'static + Add<Output = T> + Copy> TypedAction<T> for IncrBy<T> {
+impl<T: 'static + Add<Output = T> + Clone> TypedAction<T> for IncrBy<T> {
     fn bind<'x>(&self, cell: Rc<RefCell<&'x mut T>>) -> Action<'x> {
-        let IncrBy(delta) = *self;
-        return Flag(Box::new(IncrByAction { cell: cell, delta: delta }));
+        let IncrBy(ref delta) = *self;
+        return Flag(Box::new(IncrByAction { cell: cell, delta: delta.clone() }));
     }
 }
 
-impl<T: 'static + Sub<Output = T> + Copy> TypedAction<T> for DecrBy<T> {
+impl<T: 'static + Sub<Output = T> + Clone> TypedAction<T> for DecrBy<T> {
     fn bind<'x>(&self, cell: Rc<RefCell<&'x mut T>>) -> Action<'x> {
-        let DecrBy(delta) = *self;
-        return Flag(Box::new(DecrByAction { cell: cell, delta: delta }));
+        let DecrBy(ref delta) = *self;
+        return Flag(Box::new(DecrByAction { cell: cell, delta: delta.clone() }));
     }
 }
 
-impl<'a, T: Add<Output = T> + Copy> IFlagAction for IncrByAction<'a, T> {
+impl<'a, T: Add<Output = T> + Clone> IFlagAction for IncrByAction<'a, T> {
     fn parse_flag(&self) -> ParseResult {
+        let oldval = {
+            let targ = self.cell.borrow();
+            targ.clone()
+        };
         let mut targ = self.cell.borrow_mut();
-        let oldval = **targ;
-        **targ = oldval + self.delta;
+        **targ = oldval + self.delta.clone();
         return Parsed;
     }
 }
 
-impl<'a, T: Sub<Output = T> + Copy> IFlagAction for DecrByAction<'a, T> {
+impl<'a, T: Sub<Output = T> + Clone> IFlagAction for DecrByAction<'a, T> {
     fn parse_flag(&self) -> ParseResult {
+        let oldval = {
+            let targ = self.cell.borrow();
+            targ.clone()
+        };
         let mut targ = self.cell.borrow_mut();
-        let oldval = **targ;
-        **targ = oldval - self.delta;
+        **targ = oldval - self.delta.clone();
         return Parsed;
     }
 }
